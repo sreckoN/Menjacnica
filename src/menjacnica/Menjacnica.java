de6c77da79feb73c35a4d94507988e2d28faffce
sreckoN <srecko.nikolic71@hotmail.com>
2018-03-16 20:16:49 +0100

    Razvijene nasledjene metode u klasi Menjacnica

--- src/menjacnica/Menjacnica.java
+++ src/menjacnica/Menjacnica.java
@@ -1,27 +1,41 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import menjacnicaInterfejs.MenjacnicaInterfejs;
 
 public class Menjacnica implements MenjacnicaInterfejs {
+	
+	private LinkedList<Valuta> valute = new LinkedList<>();
 
 	@Override
 	public void unesiKursValuteZaDatum(String skracenNaziv, int kupovni, int prodajni, int srednji,
 			GregorianCalendar datum) {
-		// TODO Auto-generated method stub
-
+		Valuta v = new Valuta();
+		v.setSkracenNaziv(skracenNaziv);
+		v.setDatum(datum);
+		v.setProdajni(prodajni);
+		v.setKupovni(kupovni);
+		v.setSrednji(srednji);
+		if(valute.contains(v)) return;
+		valute.add(v);
 	}
 
 	@Override
 	public void brisanjeKursaValuteZaDatum(String skracenNaziv, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
-
+		for (int i = 0; i < valute.size(); i++) {
+			if(valute.get(i).getSkracenNaziv().equals(skracenNaziv) && valute.get(i).getDatum().equals(datum))
+				valute.remove(valute.get(i));
+		}
 	}
 
 	@Override
 	public double vratiKursValutePoDatumu(String skracenNaziv, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+		for (int i = 0; i < valute.size(); i++) {
+			if(valute.get(i).getSkracenNaziv().equals(skracenNaziv) && valute.get(i).getDatum().equals(datum))
+				return valute.get(i).getSrednji();
+		}
 		return 0;
 	}
 
