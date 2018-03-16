9339e59f3457dc37781e0b063b6e649d2d3989f5
sreckoN <srecko.nikolic71@hotmail.com>
2018-03-16 20:05:23 +0100

    Uvedena nefunkcionalna klasa Menjacnica koja implementira interfejs
    MenjacnicaInterfejs

---
+++ src/menjacnica/Menjacnica.java
@@ -0,0 +1,28 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import menjacnicaInterfejs.MenjacnicaInterfejs;
+
+public class Menjacnica implements MenjacnicaInterfejs {
+
+	@Override
+	public void unesiKursValuteZaDatum(String skracenNaziv, int kupovni, int prodajni, int srednji,
+			GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void brisanjeKursaValuteZaDatum(String skracenNaziv, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public double vratiKursValutePoDatumu(String skracenNaziv, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
