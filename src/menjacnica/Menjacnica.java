5b677768dd3ce66beadfa912557cd45275cdf9d3
sreckoN <srecko.nikolic71@hotmail.com>
2018-03-16 19:19:32 +0100

    Uvedene metode equals, hashCode i toString u okviru klase Valuta

--- src/menjacnica/Menjacnica.java
+++ src/menjacnica/Menjacnica.java
@@ -49,4 +49,40 @@ public class Menjacnica {
 	public void setDatum(GregorianCalendar datum) {
 		this.datum = datum;
 	}
+	@Override
+	public String toString() {
+		return "Menjacnica [nazivValute=" + nazivValute + ", skracenNaziv=" + skracenNaziv + ", prodajni=" + prodajni
+				+ ", kupovni=" + kupovni + ", srednji=" + srednji + ", datum=" + datum + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+		result = prime * result + ((skracenNaziv == null) ? 0 : skracenNaziv.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Menjacnica other = (Menjacnica) obj;
+		if (datum == null) {
+			if (other.datum != null)
+				return false;
+		} else if (!datum.equals(other.datum))
+			return false;
+		if (skracenNaziv == null) {
+			if (other.skracenNaziv != null)
+				return false;
+		} else if (!skracenNaziv.equals(other.skracenNaziv))
+			return false;
+		return true;
+	}
+	
+	
 }
