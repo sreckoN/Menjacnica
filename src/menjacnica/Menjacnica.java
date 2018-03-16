8ff3f1de33ba6a50da41ff0db3fd90a87b1b228c
sreckoN <srecko.nikolic71@hotmail.com>
2018-03-16 20:25:48 +0100

    Dodat kod u metodu za unos novog kursa

--- src/menjacnica/Menjacnica.java
+++ src/menjacnica/Menjacnica.java
@@ -1,16 +1,25 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import menjacnicaInterfejs.MenjacnicaInterfejs;
 
 public class Menjacnica implements MenjacnicaInterfejs {
+	
+	private LinkedList<Valuta> valute = new LinkedList<>();
 
 	@Override
 	public void unesiKursValuteZaDatum(String skracenNaziv, int kupovni, int prodajni, int srednji,
 			GregorianCalendar datum) {
-		// TODO Auto-generated method stub
-
+		Valuta a = new Valuta();
+		a.setSkracenNaziv(skracenNaziv);
+		a.setDatum(datum);
+		if(valute.contains(a)) return;
+		a.setKupovni(kupovni);
+		a.setProdajni(prodajni);
+		a.setSrednji(srednji);
+		valute.add(a);
 	}
 
 	@Override
